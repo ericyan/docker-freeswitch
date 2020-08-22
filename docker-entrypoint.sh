cf3632371b9ef195802ade5e50c3e88afd9682ad
Eric Yan <github@ericyan.me>
2020-08-22 15:02:46 +0200

    Do not bind to privileged ports

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,6 @@ done
 chown -R freeswitch:freeswitch /etc/freeswitch
 
 if [ "$1" = 'freeswitch' ]; then
-  setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/freeswitch
   exec chroot --userspec=freeswitch / /usr/bin/freeswitch -u freeswitch -g freeswitch -c -nonat
 fi
 
